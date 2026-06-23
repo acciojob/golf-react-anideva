c5312477ce878c4b4fce5a6e59374138a8e875b2
anideva <anidevarchowdhury@gmial.com>
2026-06-23 13:56:28 +0530

    fixed the code in the app.js (debugging)

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,71 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      renderBall: false,
+      position: 0,
     };
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  buttonClickHandler() {
+    this.setState({
+      renderBall: true,
+    });
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode === 39) {
+      this.setState((prevState) => ({
+        position: prevState.position + 5,
+      }));
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{
+            position: "relative",
+            left: `${this.state.position}px`,
+          }}
+        ></div>
+      );
     }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+    return (
+      <button
+        className="start"
+        onClick={this.buttonClickHandler}
+      >
+        Start
+      </button>
+    );
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="playground">
+        {this.renderBallOrButton()}
+      </div>
+    );
+  }
 }
-
 
 export default App;
